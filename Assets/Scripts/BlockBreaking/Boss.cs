37d878e380801df3309e4b90d0dfe9321d322687
Nikotaro0713 <nikotaro713@gmail.com>
2025-10-13 22:41:10 +0900

    2025-10-13 【ブロック崩し】第3ステージ、ボスの枠組み実装

---
+++ Assets/Scripts/BlockBreaking/Boss.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 2.0f;
+    [SerializeField] private float moveDistance = 0.5f;
+
+    private Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Mathf.PingPong(Time.time * moveSpeed, moveDistance) - (moveDistance /2f);
+        transform.position = startPosition + new Vector3(move, 0, 0);
+    }
+}
